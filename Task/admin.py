7814bc3d52ae5cb4ff877f982c0749babb06ea51
Abdelrahman Mohamed Mohamed <abdofawzi5@gmail.com>
2017-05-01 11:49:14 +0200

    add task attachments

--- Task/admin.py
+++ Task/admin.py
@@ -2,14 +2,22 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from Task import models
 
+class AttachmentInline(admin.TabularInline):
+	model = models.Attachment
+	fields = ('fk_task','file','details')
+	extra = 0
+
 class TaskAdmin(admin.ModelAdmin):
-	list_display = ('fk_app','label','name','created_at','updated_at','done')
-	search_fields = ('name','fk_lable__name','fk_app__fk_project__name','fk_app__name')
+	list_display = ('id','fk_app','label','title','created_at','updated_at','done')
+	search_fields = ('title','fk_lable__name','fk_app__fk_project__name','fk_app__name')
 	list_filter = ('created_at','updated_at','done','fk_label__name','fk_app__fk_project__name','fk_app')
+
+	inlines = [AttachmentInline,]
 
 	def label(self, obj):
 	    return '<b style="color:{};">{}</b>'.format(obj.fk_label.color, obj.fk_label.name)
 	label.allow_tags = True
+
 
 
 admin.site.register(models.Task, TaskAdmin)
--- Task/models.py
+++ Task/models.py
@@ -8,15 +8,29 @@ from Setting.models import Label
 class Task(models.Model):
 	fk_app = models.ForeignKey(App, verbose_name=_('App'), related_name='app_task')
 	fk_label = models.ForeignKey(Label, blank=True, null=True, verbose_name=_('Label'), related_name='app_label') 
-	name = models.CharField(max_length=100, verbose_name=_('Task Name'))
+	title = models.CharField(max_length=200, verbose_name=_('Title'))
 	description = models.TextField(blank=True, null=True ,verbose_name=_('Description'))
 	done = models.BooleanField(default=False ,verbose_name=_('Done'))
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
 	def __unicode__(self):
-		return unicode(self.name)
+		return str(self.id) + '-' + unicode(self.title)
 
 	class Meta:
 		verbose_name = _('Task')
 		verbose_name_plural = _('Tasks')
+
+class Attachment(models.Model):
+	fk_task = models.ForeignKey(Task, verbose_name=_('Task'), related_name='task_attachment')
+	file = models.FileField(upload_to='attachments/', verbose_name = _('File'))
+	details = models.TextField(blank=True, null=True ,verbose_name=_('Details'))
+
+	def __unicode__(self):
+		return unicode(self.file)
+
+	class Meta:
+		verbose_name = _('Attachment')
+		verbose_name_plural = _('Attachments')
+
+
