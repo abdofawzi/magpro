2bfd615014c0e96126e5c40d42f7afed941cfec3
abdelrahman <abdofawzi5@gmail.com>
2017-06-30 17:17:27 +0200

    fix User migrations

--- PM/settings.py
+++ PM/settings.py
@@ -41,10 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'colorful',
     'pagedown',
+    'User',
     'Project',
     'Task',
     'Setting',
-    'User',
     'Wiki',
 ]
 
@@ -129,9 +129,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = BASE_DIR + MEDIA_URL
 STATIC_ROOT = BASE_DIR + '/static-root/'
+
+
