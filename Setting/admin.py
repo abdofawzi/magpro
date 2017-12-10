e8cbcd2aa360338e65331cafcb6f770f30fdbcaa
abdofawzi <abdofawzi5@gmail.com>
2017-12-10 13:36:04 +0200

    disply created_at and updated_at for settings in amdin

--- Setting/admin.py
+++ Setting/admin.py
@@ -2,7 +2,26 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from Setting import models
 
-admin.site.register(models.Label)
-admin.site.register(models.Status)
-admin.site.register(models.Type)
+
+class LabelAdmin(admin.ModelAdmin):
+	list_display = ('name','created_at','updated_at',)
+	search_fields = ('created_at','updated_at',)
+	list_filter = ('created_at','updated_at',)
+
+
+class StatusAdmin(admin.ModelAdmin):
+	list_display = ('name','created_at','updated_at',)
+	search_fields = ('created_at','updated_at',)
+	list_filter = ('created_at','updated_at',)
+
+
+class TypeAdmin(admin.ModelAdmin):
+	list_display = ('name','created_at','updated_at',)
+	search_fields = ('created_at','updated_at',)
+	list_filter = ('created_at','updated_at',)
+
+
+admin.site.register(models.Label,LabelAdmin)
+admin.site.register(models.Status,StatusAdmin)
+admin.site.register(models.Type,TypeAdmin)
 
